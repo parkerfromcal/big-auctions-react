e69102c097be2b5b10d5f18cc02285ba04e91b36
parkerfromcal <kparker.adrienne@gmail.com>
2021-04-23 10:14:39 -0400

    attempt to fix about component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
-import About from "./About";
+import About from "./About.jsx";
 import { Switch, Route } from "react-router-dom";
 
 class Main extends Component {
