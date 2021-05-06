b7a0c5ab2f383bb96bba6a4dee005bf142d97f61
parkerfromcal <kparker.adrienne@gmail.com>
2021-05-06 11:46:28 -0400

    accordions

--- src/components/Buy.jsx
+++ src/components/Buy.jsx
@@ -5,6 +5,8 @@ import { FORSALE } from "../shared/forSale";
 import { AUCTIONS } from "../shared/auctions";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 
 class Buy extends Component {
   constructor(props) {
@@ -35,7 +37,7 @@ class Buy extends Component {
           </div>
         </div>
         <div className="row justify-content-center">
-          <div className="col-10 shadow">
+          <div className="col-10 shadow mb-5">
             <Tabs defaultActiveKey="auctions" id="buyTabs">
               <Tab eventKey="auctions" title="auctions">
                 <Auctions auctions={this.state.auctions} />
@@ -48,9 +50,48 @@ class Buy extends Component {
         </div>
         <div className="container">
           <div className="row flex justify-content-center">
-            <div className="col-12 col-md-6 mt-5 mb-5">
-              <h3>Frequently Asked Questions - Coming Soon</h3>
-              <p>Accordions go here</p>
+            <div className="col-12 mt-5 mb-8">
+              <h2 className="text-center mb-5">Frequently Asked Questions</h2>
+              <Accordion defaultActiveKey="0">
+                <Card>
+                  <Accordion.Toggle as={Card.Header} eventKey="0">
+                    Why do I need a Big Auctions account?
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                      Creating an account with Big Auctions lets you register
+                      for upcoming auctions and bid for items online. You can
+                      create a profile and keep track of your Big Auctions
+                      activities. And if you're interested in selling, you can
+                      easily connect with us and get your items online.
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Accordion.Toggle as={Card.Header} eventKey="1">
+                    I am interested in selling. How can I get in touch?
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>
+                      Fill out our <a href="/sell">Interested in Selling</a>{" "}
+                      form and someone will contact you soon.
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Accordion.Toggle as={Card.Header} eventKey="2">
+                    Do you host auctions in person or only online?
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="2">
+                    <Card.Body>
+                      Big Auctions hosts auctions across the country. Since
+                      March 2020, we have moved all our auctions online. We are
+                      proud of our virtual auction experience and hope to be
+                      back in person in the next 6 months.
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
             </div>
           </div>
         </div>
